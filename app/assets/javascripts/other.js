b147e653b264c7fbfa3cadd5bf131281dd2ddfe1
Jeffrey Snowiss <jasnowiss@gmail.com>
2016-08-03 23:36:30 -0700

    simple dropdowns working

--- app/assets/javascripts/other.js
+++ app/assets/javascripts/other.js
@@ -105,4 +105,28 @@ $(document).ready(function() {
         return ((r << 16) | (g << 8) | b).toString(16);
     }
 
+    /*** SECTION FOR DROPDOWN MENUS ***/
+    $(".dropdown-menu > li > a.trigger").on("click",function(e){
+        var current=$(this).next();
+        var grandparent=$(this).parent().parent();
+        if($(this).hasClass('left-caret')||$(this).hasClass('right-caret'))
+            $(this).toggleClass('right-caret left-caret');
+        grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
+        grandparent.find(".sub-menu:visible").not(current).hide();
+        current.toggle();
+        e.stopPropagation();
+    });
+    $(".dropdown-menu > li > a:not(.trigger)").on("click",function(){
+        var root=$(this).closest('.dropdown');
+        root.find('.left-caret').toggleClass('right-caret left-caret');
+        root.find('.sub-menu:visible').hide();
+    });
+
+
+
 });
+
+/*** SECTION FOR DROPDOWN RENAMING ***/
+function rename(s) {
+    document.getElementById("ca-county").innerHTML = s;
+}
