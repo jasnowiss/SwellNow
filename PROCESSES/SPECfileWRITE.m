43ad043084c7c3d294d6c06a0a29b155300b330e
Jeffrey Snowiss <jasnowiss@gmail.com>
2016-08-15 16:27:36 -0700

    mcode stuff

---
+++ PROCESSES/SPECfileWRITE.m
@@ -0,0 +1,46 @@
+clc;clear all;close all; fclose all
+
+[date, bw, E, f]=specsum('.\inputs\DATA_spec.txt');
+[date, bw, A1, f]=specsum('.\inputs\DATA_dir1.txt');
+[date, bw, A2, f]=specsum('.\inputs\DATA_dir2.txt');
+[date, bw, R1, f]=specsum('.\inputs\DATA_r1.txt');
+[date, bw, R2, f]=specsum('.\inputs\DATA_r2.txt');
+
+
+%convert direction
+Dir=-1*((A1(1,:)+A2(1,:))./2)+270;
+    
+%openfile in SWAN directory
+
+fid=fopen('.\inputs\specfiletemplate.txt','r');
+
+for i=1:1:43
+ 
+    header{i}=fgetline(fid);
+    
+end
+
+%create enter symbol to clear new line
+rt=header{43};
+
+fid2=fopen('.\inputs\specSWANfile.txt','w');
+
+for i=1:1:42
+ 
+    fwrite(fid2,header{i},'char');
+    
+end
+
+fwrite(fid2, ['LOCATION 1'],'char');
+fwrite(fid2, [rt],'char');
+
+for i=1:1:25
+    Energy=num2str(E(1,i));
+    NDIR=num2str(Dir(i));
+    DSPR=num2str(abs(180/pi*(R1(1,i)-R2(1,i))));
+    fwrite(fid2, [Energy,' ',NDIR,' ',DSPR, rt],'char');
+    
+end
+
+    fclose all
+    
---
+++ PROCESSES/TEST1.m
@@ -0,0 +1,64 @@
+clc;clear all
+%input dimensions
+N=241; M=181;
+
+
+fid1=fopen('.\output\HsCA.txt');
+
+
+    H=fscanf(fid1,'%f',[N M]); % xc+1 and yc+1
+     AA=find(H<0.01);
+    H(AA)=NaN;
+    
+%convert from m to ft. 
+
+H=H.*3.28084;    
+
+
+figure
+ sizefig(500,500)
+ 
+ 
+ chart=pcolor(H')
+ 
+ set(chart,'edgecolor','flat')
+ axis equal
+axis off
+ 
+ %create colormap
+ 
+ a=linspace(pi/80,pi/2,40);
+ b=linspace(pi/2+pi/80,pi,40);
+ c=linspace(pi+pi/80,3*pi/2,40);
+ d=linspace(3*pi/2+pi/80,2*pi,40);
+ LB=zeros(1,40)
+ B=zeros(1,5);
+ T=ones(1,5)
+ 
+ 
+ uT=.5+.5*sin(a);
+ dT=.5+.5*sin(b);
+ dB=.5+.5*sin(c);
+ uB=.5+.5*sin(d);
+ 
+ red=[uT,T,dT,dB,B,uB,uT];
+ green=[B,uB,uT,T,dT,dB,LB];
+ blue=[LB,B,uB,uT,T,dT,dB];
+
+ 
+
+ color2=[blue',green',red']
+ 
+ 
+ for i=1:150
+     color(i,:)=color2(i+20,:);
+ end
+ 
+ colormap(color)
+
+clim([0 15])
+
+
+
+ set(gca,'position',[0 0 1 1] ,'units','normalized')
+  
