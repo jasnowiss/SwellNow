9ad2df4ffdac774badea806d183760ff699f22e8
Jeffrey Snowiss <jasnowiss@gmail.com>
2016-08-17 16:19:44 -0700

    code prettyfying

--- processes/SPECfileWRITE.m
+++ processes/SPECfileWRITE.m
@@ -8,39 +8,30 @@ clc;clear all;close all; fclose all
 
 
 %convert direction
-Dir=-1*((A1(1,:)+A2(1,:))./2)+270;
+Dir = -1*((A1(1,:)+A2(1,:))./2)+270;
     
 %openfile in SWAN directory
+fid = fopen('./inputs/specfiletemplate.txt','r');
 
-fid=fopen('./inputs/specfiletemplate.txt','r');
-
-for i=1:1:43
- 
-    header{i}=fgetline(fid);
-    
+for i = 1:1:43
+    header{i} = fgetline(fid);
 end
 
 %create enter symbol to clear new line
-rt=header{43};
+rt = header{43};
 
-fid2=fopen('./inputs/specSWANfile.txt','w');
+fid2 = fopen('./inputs/specSWANfile.txt','w');
 
-for i=1:1:42
- 
+for i = 1:1:42
     fwrite(fid2,header{i},'char');
-    
 end
 
 fwrite(fid2, ['LOCATION 1'],'char');
 fwrite(fid2, [rt],'char');
 
-for i=1:1:25
-    Energy=num2str(E(1,i));
-    NDIR=num2str(Dir(i));
-    DSPR=num2str(abs(180/pi*(R1(1,i)-R2(1,i))));
+for i = 1:1:25
+    Energy = num2str(E(1,i));
+    NDIR = num2str(Dir(i));
+    DSPR = num2str(abs(180/pi*(R1(1,i)-R2(1,i))));
     fwrite(fid2, [Energy,' ',NDIR,' ',DSPR, rt],'char');
-    
 end
-
-    fclose all
-    
--- processes/specsum.m
+++ processes/specsum.m
@@ -4,38 +4,27 @@ function [date, bw, Dat, f] = specsum(fname);
 %Dat=data from file of interest... could be Energy, A1, R1, etc. 
 %f=frequency
     
-fid=fopen(fname);
+fid = fopen(fname);
 fgetline(fid);
 
 % starts at Hour zero (AKA... now)
-% 
+for t = 1:1:24    
+    H(t,:) = str2num(fgetline(fid));
+    %[year, month, day, hour, minute]
+    date = H(t,1:5);
 
-for t=1:1:24
-    
-H(t,:)=str2num(fgetline(fid));
+    %DATA_spec file is different, shift is equal to 6 in that instance. otherwise, it is 5. 
+    shift = 1;
+    if H(1,6) == 9.999
+        shift = 0;
+    end
 
-
-%[year, month, day, hour, minute]
-date=H(t,1:5);
-
-
-%DATA_spec file is different, shift is equal to 6 in that instance. otherwise, it is 5. 
-shift=1;
-if H(1,6)==9.999
-shift=0;
-end
-
-
-%only choosing 25 bands for now, can change to 46 later...
-skip=5;
-for i=1:1:25
-    
-    
-    %this might look rather complicated, but it simply fills in the wave info for each location
-    Dat(t,i)=H(t,2*(i+skip)+5-shift);
-    f(i)=H(t,2*(i+skip)+6-shift);
-    bw(i)=H(t,2*(i+skip+1)+6-shift)-H(t,2*(i+skip)+6-shift);
-    
-end
-
+    %only choosing 25 bands for now, can change to 46 later...
+    skip = 5;
+    for i = 1:1:25
+        %this might look rather complicated, but it simply fills in the wave info for each location
+        Dat(t,i) = H(t,2*(i+skip)+5-shift);
+        f(i) = H(t,2*(i+skip)+6-shift);
+        bw(i) = H(t,2*(i+skip+1)+6-shift)-H(t,2*(i+skip)+6-shift);
+    end
 end
