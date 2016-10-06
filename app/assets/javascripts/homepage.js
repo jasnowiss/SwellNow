f8e78584d78835944bca4ea0df2923b7a5c8b271
Jeffrey Snowiss <jasnowiss@gmail.com>
2016-10-06 15:05:04 -0700

    dynamic nav bar and image change

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,61 +1,178 @@
 /*** JAVASCRIPT AND JQUERY FOR HOMEPAGE ***/
 
+/*** 1. MAIN IDENTIFIER, 2...END. SUB-IDENTIFIERS ***/
+var locations = ["North America", 
+["Southern California", ["Santa Barbara", "Los Angeles", "Orange County", "San Diego"]], 
+["Rhode Island", ["West Bay", "East Bay"]]];
+
+var locationurls = {"North America":"", 
+"Southern California":"", 
+"Santa Barbara":"https://dl.dropboxusercontent.com/s/4ervq1me95vdnw3/SB.png",
+"Los Angeles":"https://dl.dropboxusercontent.com/s/f4h3zen6wi5it5y/LA.png",
+"Orange County":"https://dl.dropboxusercontent.com/s/ukgy4pbum1rc0f3/OC.png",
+"San Diego":"https://dl.dropboxusercontent.com/s/9oxio9ns6mizcq7/SD.png",
+"Rhode Island":"",
+"West Bay":"https://dl.dropboxusercontent.com/s/fcvvui1npynxgkk/WestRI.png",
+"East Bay":"https://dl.dropboxusercontent.com/s/x806t0i87pjahpj/EastRI.png"};
+
 /*** SECTION FOR RESIZING ***/
 // resize the canvas to fill browser window dynamically
-window.addEventListener('resize', resizeObjs, false);
+// window.addEventListener('resize', resizeObjs, false);
 
-function resizeObjs() {
-    resizeCanvas();
-}
+// function resizeObjs() {
+//     resizeCanvas();
+// }
 
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+var defWidth = document.getElementById('chart').offsetWidth;
 
-function resizeCanvas() {
-        // var chartSize = document.getElementById('chart').offsetWidth * 2.0 / 3.0;
-        var chartSize = document.getElementById('chart').offsetWidth;
-        canvas.width = chartSize;
-        canvas.height = chartSize / 3.0;
-        canvas.style.cursor = "crosshair";
+// function resizeCanvas() {
+//         // var chartSize = document.getElementById('chart').offsetWidth * 2.0 / 3.0;
+//         var chartSize = document.getElementById('chart').offsetWidth;
+//         canvas.width = chartSize;
+//         canvas.height = chartSize / 3.0;
+//         canvas.style.cursor = "crosshair";
 
-        /**
-         * Your drawings need to be inside this function otherwise they will be reset when 
-         * you resize the browser window and the canvas goes will be cleared.
-         */
-        drawStuff(); 
+//         /**
+//          * Your drawings need to be inside this function otherwise they will be reset when 
+//          * you resize the browser window and the canvas goes will be cleared.
+//          */
+//         drawCanvas(locationurls.Los_Angeles); 
+// }
+
+function getLocUrl(str) {
+    return locationurls[str];
 }
 
-function drawStuff() {
+function drawCanvas(str) {
     // context.fillStyle = "rgb(255,0,0)";
     // context.fillRect(0, 0, 50, 50);
     // context.fillStyle = "rgb(0,0,255)";
     // context.fillRect(55, 0, 50, 50);
     // var chartSize = document.getElementById('chart').offsetWidth * 2.0 / 3.0;
-    var chartSize = document.getElementById('chart').offsetWidth;
     var base_image = new Image();
     base_image.setAttribute('crossOrigin', 'Anonymous');
-    base_image.src = 'https://dl.dropboxusercontent.com/s/fcvvui1npynxgkk/WestRI.png';
+    base_image.src = getLocUrl(str);
     base_image.onload = function(){
-        context.drawImage(base_image, 0, 0, chartSize, chartSize * base_image.height / base_image.width);
+        var ratio = base_image.width / base_image.height;
+        var chartSize = defWidth;
+        canvas.width = chartSize;
+        canvas.height = chartSize / ratio;
+        canvas.style.cursor = "crosshair";
+        context.drawImage(base_image, 0, 0, chartSize, chartSize / ratio);
     }
 }
 
-resizeObjs();
+// function setup() {
+//     drawCanvas("West Bay");
+//     document.getElementById('swellcolors').width = document.getElementById('chart').offsetWidth;
+// }
+
+// resizeObjs();
+// setup();
+
+// FOR TESTING. REMOVE LATER
+// $('<div/>', {
+//     'id':'myDiv',
+//     'class':'myClass',
+//     'text':'Text Only',
+// }).on('click', function(){
+//     alert(this.id);
+// }).appendTo('body');
+
+/*** TAKES IN A STRING AND CONSTRUCTS THE NAVIGATION TABS ***/
+function createNavHTML(str) {
+    createNavHTMLHelper(getLocPath(str));
+}
+
+function createNavHTMLHelper(ar) {
+    var html = "";
+
+    html += "<div id='dd-menu1' class='dropdown' style='position:relative;display:inline-block'>";
+    html += "<a href='#' class='btn btn-primary dropdown-toggle' data-toggle='dropdown'>" + locations[0] + " <span class='caret'></span></a>";
+    html += "<ul class='dropdown-menu'>";
+    for (var i = 1; i < locations.length; i++) {
+        html += "<li>";
+        html += "<a class='trigger right-caret'>" + locations[i][0] + "</a>";
+        html += "<ul class='dropdown-menu sub-menu'>";
+        for (var j = 0; j < locations[i][1].length; j++) {
+            html += "<li><a href='#' onclick='setNavAndCanvas(&#39;" + locations[i][1][j] + "&#39;);return false;'>" + locations[i][1][j] + "</a></li>";
+        }
+        html += "</ul>";
+        html += "</li>";
+    }
+    html += "</ul>";
+    html += "</div>";
+
+    html += "<div class='dropdown' style='position:relative;display:inline-block;margin-left:5px'>";
+    html += "<a href='#' class='btn btn-primary dropdown-toggle' data-toggle='dropdown'>" + locations[ar[0]][0] + " <span class='caret'></span></a>";
+    html += "<ul class='dropdown-menu'>";
+    for (var k = 0; k < locations[ar[0]][ar[1]].length; k++) {
+        html += "<li><a href='#' onclick='setNavAndCanvas(&#39;" + locations[ar[0]][ar[1]][k] + "&#39;);return false;'>" + locations[ar[0]][ar[1]][k] + "</a></li>";
+    }
+    html += "</ul>";
+    html += "</div>";
+    
+    html += "<div class='dropdown' style='position:relative;display:inline-block;margin-left:5px'>";
+    html += "<a href='#' class='btn btn-primary' onclick='setNavAndCanvas(&#39;" + locations[ar[0]][ar[1]][ar[2]] + "&#39;);return false;'>" + locations[ar[0]][ar[1]][ar[2]] + "</a>";
+    html += "</div>";
+
+    $('#location-text').html("<p>Displaying swell for " + locations[ar[0]][ar[1]][ar[2]] + "</p>");
+    $('#dropmenu').html(html);
+}
+
+/*** GETS THE LOCATION PATH OF A STRING IN TERMS OF INDEXES TRAVELED ***/
+function getLocPath(str) {
+    return getLocPathHelper(str, locations, []);
+}
+
+function getLocPathHelper(str, ar1, ar2) {
+    for (var i = 0; i < ar1.length; i++) {
+        var arCopy = ar2.slice();
+        if (ar1[i] == str) {
+            arCopy.push(i);
+            return arCopy;
+        } else if (ar1[i].constructor == Array) {
+            arCopy.push(i);
+            var result = getLocPathHelper(str, ar1[i], arCopy);
+            if (result.length != 0) {
+                return result;
+            }
+        }
+    }
+    return [];
+}
+
+function setNavAndCanvas(str) {
+    createNavHTML(str);
+    drawCanvas(str);
+    document.getElementById('swellcolors').width = defWidth;
+    resetDropdown();
+    resetCanvasSensing();
+}
+
+function spaceToUnderscore(str) {
+    return str.replace(/\s/g, "_");
+}
+
+function underscoreToSpace(str) {
+    return str.replace(/_/g, " ");
+}
 
 /*** SECTION FOR COLOR SENSING ***/
-$('#canvas').mousemove(function(e) {
-    var pos = findPos(this);
-    var x = e.pageX - pos.x;
-    var y = e.pageY - pos.y;
-    var coord = "x=" + x + ", y=" + y;
-    var c = this.getContext('2d');
-    var p = c.getImageData(x, y, 1, 1).data; 
-    var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-    // $('#status').html(coord + "</br>" + hex);
-});
-
-// FOR TESTING
-$('#location-text').html("</br>" + "Displaying swell for Rhode Island, West Bay");
+function resetCanvasSensing() {
+    $('#canvas').mousemove(function(e) {
+        var pos = findPos(this);
+        var x = e.pageX - pos.x;
+        var y = e.pageY - pos.y;
+        var coord = "x=" + x + ", y=" + y;
+        var c = this.getContext('2d');
+        var p = c.getImageData(x, y, 1, 1).data; 
+        var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+        // $('#status').html(coord + "</br>" + hex);
+    });
+}
 
 function findPos(obj) {
     var curleft = 0, curtop = 0;
@@ -70,76 +187,33 @@ function findPos(obj) {
 }
 
 function rgbToHex(r, g, b) {
-    if (r > 255 || g > 255 || b > 255)
+    if (r > 255 || g > 255 || b > 255) {
         throw "Invalid color component";
+    }
     return ((r << 16) | (g << 8) | b).toString(16);
 }
 
 /*** SECTION FOR DROPDOWN MENUS ***/
-$(".dropdown-menu > li > a.trigger").on("click",function(e){
-    var current=$(this).next();
-    var grandparent=$(this).parent().parent();
-    if($(this).hasClass('left-caret')||$(this).hasClass('right-caret'))
-        $(this).toggleClass('right-caret left-caret');
-    grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
-    grandparent.find(".sub-menu:visible").not(current).hide();
-    current.toggle();
-    e.stopPropagation();
-});
-$(".dropdown-menu > li > a:not(.trigger)").on("click",function(){
-    var root=$(this).closest('.dropdown');
-    root.find('.left-caret').toggleClass('right-caret left-caret');
-    root.find('.sub-menu:visible').hide();
-});
-
-/*** SECTION FOR DROPDOWN RENAMING ***/
-function rename(s) {
-    document.getElementById("ca-county").innerHTML = s;
-}
-
-// FOR TESTING. REMOVE LATER
-// $('<div/>', {
-//     'id':'myDiv',
-//     'class':'myClass',
-//     'text':'Text Only',
-// }).on('click', function(){
-//     alert(this.id);
-// }).appendTo('body');
-
-/*** 1. MAIN IDENTIFIER, 2...END. SUB-IDENTIFIERS ***/
-var locations = ["North America", 
-["West Coast", ["Southern California", ["Santa Barbara", "Los Angeles", "Orange County", "San Diego"]]], 
-["East Coast", ["Rhode Island", ["West Bay", "East Bay"]]]];
-
-/*** TAKES IN A STRING AND CONSTRUCTS THE NAVIGATION TABS ***/
-function createNavHTML(s) {
-    createNavHTMLHelper(getLocPath(s));
-}
-
-function createNavHTMLHelper(ar) {
-
-}
-
-/*** GETS THE LOCATION PATH OF A STRING IN TERMS OF INDEXES TRAVELED ***/
-function getLocPath(s) {
-    return getLocPathHelper(s, locations, []);
-}
-
-function getLocPathHelper(s, ar1, ar2) {
-    for (var i = 0; i < ar1.length; i++) {
-        var arCopy = ar2.slice();
-        if (ar1[i] == s) {
-            arCopy.push(i);
-            return arCopy;
-        } else if (ar1[i].constructor == Array) {
-            arCopy.push(i);
-            var result = getLocPathHelper(s, ar1[i], arCopy);
-            if (result.length != 0) {
-                return result;
-            }
+function resetDropdown() {
+    $(".dropdown-menu > li > a.trigger").on("click",function(e){
+        var current=$(this).next();
+        var grandparent=$(this).parent().parent();
+        if($(this).hasClass('left-caret')||$(this).hasClass('right-caret')) {
+            $(this).toggleClass('right-caret left-caret');
         }
-    }
-    return [];
+        grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
+        grandparent.find(".sub-menu:visible").not(current).hide();
+        current.toggle();
+        e.stopPropagation();
+    });
+    $(".dropdown-menu > li > a:not(.trigger)").on("click",function(){
+        var root=$(this).closest('.dropdown');
+        root.find('.left-caret').toggleClass('right-caret left-caret');
+        root.find('.sub-menu:visible').hide();
+    });
 }
+
+setNavAndCanvas("West Bay");
 
 // alert(getLocPath("West Bay"));
+// alert(spaceToUnderscore(locationurls.North_America));
