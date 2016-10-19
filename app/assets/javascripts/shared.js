ccd12de59415fca2b36480a547ef499560606528
Jeffrey Snowiss <jasnowiss@gmail.com>
2016-10-18 18:22:09 -0700

    horizontal navbar scroll

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -1,2 +1,26 @@
 /*** FOR FUNCTIONALITY SHARED BY ALL WEBPAGES ***/
 
+/*** FOR HORIZONTAL BUT NOT VERTICALLY SCROLLING NAVBAR ***/
+function setTopBar() {
+    var $horizontal = $('#horizontal');
+
+    $(window).scroll(function () {
+        var s = $(this).scrollLeft(),
+            d = $(document).width(),
+            c = $(this).width();
+
+        scrollPercent = (s / (d - c));
+
+        var position = (scrollPercent * ($(document).width() - $horizontal.width()));
+        
+        $horizontal.css({
+            'left': position
+        });
+    });
+}
+
+function initShared() {
+    setTopBar();
+}
+
+initShared();
